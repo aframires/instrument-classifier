de34f50bc2a6d4cca4dac5e7ccc2e83f6692d830
Antonio Ramires <aframires@gmail.com>
2019-04-17 17:11:01 +0200

    XAdded the updated versions for DAFx paper

--- src/models/singleLayerMM3.py
+++ src/models/singleLayerMM3.py
@@ -3,17 +3,104 @@ import tensorflow as tf
 
 from models.model_parent_class import ModelsParentClass
 
+class ValCallback(tf.keras.callbacks.Callback):
+
+   def __init__(self, val_sets):
+
+
+       self.val_data = val_sets
+
+
+
+   def on_epoch_end(self, epoch, logs={}):
+
+        for dataset in self.val_data:
+
+            metrics = self.model.evaluate(dataset['data'],steps=12678,verbose=0) #12678
+
+            for metric in range(len(metrics)):
+                print('Dataset ' + dataset['name'] + '\tEpoch ' + str(epoch) + '\t' + self.model.metrics_names[metric] + ' ' + str(metrics[metric]))
+
+
+
+
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true * y_pred, 0, 1)))
+        possible_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + tf.keras.backend.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + tf.keras.backend.epsilon())
+        return precision
+
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2 * ((precision * recall) / (precision + recall + tf.keras.backend.epsilon()))
+
+
+def precision(y_true, y_pred):
+    """Precision metric.
+
+    Only computes a batch-wise average of precision.
+
+    Computes the precision, a metric for multi-label classification of
+    how many selected items are relevant.
+    """
+    true_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + tf.keras.backend.epsilon())
+    return precision
+
+
+def recall(y_true, y_pred):
+    """Recall metric.
+
+    Only computes a batch-wise average of recall.
+
+    Computes the recall, a metric for multi-label classification of
+    how many relevant items are selected.
+    """
+    true_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true * y_pred, 0, 1)))
+    possible_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + tf.keras.backend.epsilon())
+    return recall
+
+
+
+
+
+
+
+
+
 
 class SingleLayerMM3(ModelsParentClass):
 
     N_MEL_BANDS = 80
     SEGMENT_DUR = 247
     SHUFFLE_BUFFER = 1000
-    EARLY_STOPPING_EPOCH = 20
-    SGD_LR_REDUCE = 10
+    EARLY_STOPPING_EPOCH = 30
     init_lr = 0.001
 
-    set_of_effects = set(['bitcrusher','chorus','delay','flanger','reverb','tube','pitch_shifting'])
+    set_of_effects = set(['bitcrusher','chorus','delay','flanger','reverb','tube'])
 
     def __init__(self, learning_rate, training, nb_classes, num_epochs, train_iters, valid_iters, batch_size,
                  train_data, valid_data,datasets):
@@ -27,7 +114,7 @@ class SingleLayerMM3(ModelsParentClass):
         self.batch_size = batch_size
         self.train_data = train_data
         self.valid_data = valid_data
-        self.optimizer = tf.keras.optimizers.SGD(lr=self.init_lr, momentum=0.9, nesterov=True)
+        self.optimizer = tf.keras.optimizers.Adam(lr=self.init_lr)
         self.datasets = datasets
         super()
 
@@ -82,16 +169,32 @@ class SingleLayerMM3(ModelsParentClass):
         early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=self.EARLY_STOPPING_EPOCH)
         train_dataset = self.create_dataset(self.train_data)
         val_dataset = self.create_dataset(self.valid_data)
+        dataset_dict = [{'name': 'none', 'data': val_dataset}]
 
 
         if self.datasets == 'all':
             index_of_train = self.train_data.find("-spec")
+            self.train_iters = self.train_iters * (1 + len(self.set_of_effects))
             for effect in self.set_of_effects:
                 extended_data_path = self.train_data[:index_of_train] + '-' + effect + self.train_data[index_of_train:]
                 extended_dataset = self.create_dataset(extended_data_path)
                 train_dataset.concatenate(extended_dataset)
                 train_dataset.shuffle(self.SHUFFLE_BUFFER)
-                self.train_iters = self.train_iters * len(self.set_of_effects)
+
+                print('Added all effects')
+
+        elif self.datasets == 'all-valid':
+            index_of_valid = self.valid_data.find("-spec")
+            self.valid_iters = self.valid_iters * (1 + len(self.set_of_effects))
+            dataset_dict = [{'name': 'none', 'data': val_dataset}]
+            for effect in self.set_of_effects:
+
+                extended_valid_path = self.valid_data[:index_of_valid] + '-' + effect + self.valid_data[
+                                                                                               index_of_valid:]
+                valid_effect_dataset = self.create_dataset(extended_valid_path)
+                dataset_to_add = {'name': effect, 'data': valid_effect_dataset}
+                dataset_dict.append(dataset_to_add)
+
                 print('Added all effects')
 
         elif self.datasets in self.set_of_effects:
@@ -101,30 +204,34 @@ class SingleLayerMM3(ModelsParentClass):
             train_dataset.concatenate(extended_dataset)
             train_dataset.shuffle(self.SHUFFLE_BUFFER)
             self.train_iters = self.train_iters * 2
+
+            index_of_valid = self.valid_data.find("-spec")
+            extended_valid_path = self.valid_data[:index_of_valid] + '-' + self.datasets + self.valid_data[index_of_valid:]
+            valid_effect_dataset = self.create_dataset(extended_valid_path)
+            dataset_dict = [{'name':'none','data':val_dataset},{'name':self.datasets,'data':valid_effect_dataset}]
+            self.valid_iters = self.valid_iters * 2
             print('Added ' + self.datasets)
 
         model = self.vertical_filter_model()
         save_clb = tf.keras.callbacks.ModelCheckpoint(
             './nsynth_binary_classifier_' + self.datasets + "_epoch.{epoch:02d}-val_loss.{val_loss:.3f}",
             monitor='val_loss',
-            save_best_only=True)
-        lrs = tf.keras.callbacks.LearningRateScheduler(lambda epoch_n: self.init_lr / (2 ** (epoch_n // self.SGD_LR_REDUCE)))
+            save_best_only=False)
+        validation_saver = ValCallback(dataset_dict)
         model.summary()
         model.compile(optimizer=self.optimizer,
                       loss='categorical_crossentropy',
-                      metrics=['accuracy']
-                      #target_tensors=[label]
+                      metrics=['accuracy', f1, precision, recall]
                       )
 
         history = model.fit(train_dataset,
                             steps_per_epoch=self.train_iters,
                             epochs=self.num_epochs,
                             verbose=2,
-                            callbacks=[save_clb, early_stopping, lrs],
+                            callbacks=[save_clb, early_stopping,validation_saver],
                             validation_data=val_dataset,
                             validation_steps=self.valid_iters)
 
-        #pickle.dump(history.history, open('./history_vertical_filter_model.pkl', 'wb'))
 
         return
 
